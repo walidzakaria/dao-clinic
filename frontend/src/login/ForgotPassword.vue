223aafa327403debdf4e10af715aae671e426ae7
Walid <walidpiano@yahoo.com>
2020-12-16 08:58:52 +0200

    add password reset

--- frontend/src/login/ForgotPassword.vue
+++ frontend/src/login/ForgotPassword.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="vue-tempalte">
-        <form>
+        <form v-on:submit.prevent @submit="applyReset()">
             <h3>Forgot Password</h3>
 
             <div class="form-group">
-                <label>Email address</label>
-                <input type="email" class="form-control form-control-lg" />
+                <label>Email address <span class="text-danger">*</span></label>
+                <input v-model.trim="email" type="email"
+                  class="form-control form-control-lg" required/>
             </div>
-
+            <div v-if="submitted.isSubmitted">
+              <p v-if="submitted.isSuccess" class="text-success">
+                We sent a reset link to your email.
+                <br>
+                Or you can visit our <router-link to="/">home</router-link> page.
+              </p>
+              <p v-if="!submitted.isSuccess" class="text-danger">
+                Something went wrong :-(
+                  <br>Please try another email!
+              </p>
+            </div>
             <button type="submit" class="btn btn-dark btn-lg btn-block">Reset password</button>
 
         </form>
@@ -15,9 +26,39 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
-    return {};
+    return {
+      submitted: {
+        isSuccess: true,
+        isSubmitted: false,
+      },
+      email: '',
+    };
+  },
+  methods: {
+    applyReset() {
+      const requestBody = {
+        email: this.email,
+      };
+
+      axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8000/api/auth/users/reset_password/',
+        data: requestBody,
+      }).then((result) => {
+        console.log(result);
+        this.submitted.isSuccess = true;
+        this.submitted.isSubmitted = true;
+      }).catch((error) => {
+        const result = error.response.data;
+        console.log(result);
+        this.submitted.isSuccess = false;
+        this.submitted.isSubmitted = true;
+      });
+    },
   },
 };
 </script>
