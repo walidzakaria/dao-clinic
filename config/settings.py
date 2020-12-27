0622d247444aec2a93230bf1b8b6505a87cb67ef
Walid <walidpiano@yahoo.com>
2020-12-27 21:52:35 +0200

    configure redis

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEV = True
+DEV = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = False
