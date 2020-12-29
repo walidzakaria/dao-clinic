40b5af2039c8f818d22f045d8db7c45b73f21683
Walid <walidpiano@yahoo.com>
2020-12-29 21:09:05 +0200

    fix env file

--- config/settings.py
+++ config/settings.py
@@ -10,16 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import environ
+# import environ
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-env = environ.Env()
+# env = environ.Env()
 # reading .env file
-environ.Env.read_env()
+# environ.Env.read_env()
 
 
 
