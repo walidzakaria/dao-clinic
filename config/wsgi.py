bee7723b3ce25dfb2379d92209601b94c25348cc
Walid <walidpiano@yahoo.com>
2020-12-29 19:31:52 +0200

    fix wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
 
 application = get_wsgi_application()
