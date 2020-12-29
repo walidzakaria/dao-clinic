a5bf935595b896f68029648efe05ad662b6c4143
Walid <walidpiano@yahoo.com>
2020-12-29 23:25:35 +0200

    fix asgi

--- config/asgi.py
+++ config/asgi.py
@@ -1,26 +1,13 @@
 """
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
 """
-
-
-# import os
-
-# from django.core.asgi import get_asgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-#
-# application = get_asgi_application()
 
 import os
 import django
-
+from decouple import config
 from channels.routing import get_default_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'{config("PROJECT_NAME")}.settings')
 django.setup()
 application = get_default_application()
