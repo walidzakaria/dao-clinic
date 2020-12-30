47bcaec28a17aae390244815509b63dd20588bcd
Walid <walidpiano@yahoo.com>
2020-12-30 11:18:13 +0200

    fix static

--- config/settings.py
+++ config/settings.py
@@ -250,8 +250,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-# STATIC_URL = '/static/'
+# STATIC_URL = '%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = '/static/'
 
 TEMP = os.path.join(BASE_DIR, 'temp')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
