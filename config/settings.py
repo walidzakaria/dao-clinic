e995335cd23e5967492d9689fdeac8f25e773b58
Walid <walidpiano@yahoo.com>
2021-02-02 23:29:56 +0200

    fix popup login

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
