0f3e72ba572386cb786e315ed22f97a122e02a7a
Zakaria <walid.zakaria@redsea24.com>
2022-11-01 13:08:00 +0200

    separate fe from be

--- frontend/src/views/ContactUs.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="contact-us">
-    <h1>This is contact us page</h1>
-  </div>
-</template>
--- frontend/src/views/Prices.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="prices">
-    <h1>This is prices page</h1>
-  </div>
-</template>
